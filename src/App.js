a44eb09473311c9d8213a3e227e383c894b6ca20
Veroika <17veronikag@gmail.com>
2022-03-28 23:28:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { Component, useState } from 'react';
+import Clothes from './Clothes';
+import { data } from './data';
 import './App.css';
+import Buttons from './Buttons';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [clothes, setClothes] = useState(data);
+
+  const chosenClothes = (searchTerm) => {
+    const newClothes = data.filter(element => element.searchTerm === searchTerm);
+    setClothes(newClothes);
+  }
+
+  return(
+    <div>
+      <div className='cont'>
+        <h2 className='back'>Free Standard Shipping</h2>
+      </div>
+      <Buttons filteredClothes={chosenClothes}/>
+      <Clothes itemForSale={clothes}/>
     </div>
-  );
+  )
+
+
 }
 
 export default App;
